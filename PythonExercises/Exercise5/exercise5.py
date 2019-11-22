c8f8967df0c46c2157e1f8e5eb54330f11cedc03
Zois Zogopoulos <zoiszogop@gmail.com>
2019-11-22 11:26:48 +0200

    develop5_v2
    -Reworked random length of list generator.
    -Added comments.

--- PythonExercises/Exercise5/exercise5.py
+++ PythonExercises/Exercise5/exercise5.py
@@ -2,20 +2,21 @@
 
 import random
 
-a_len = random.sample(range(10, 30), 1)[0]
-b_len = random.sample(range(10, 30), 1)[0]
+a_len = random.randrange(20)  # Generate a random integer to represent the length of list a.
+b_len = random.randrange(20)  # Generate a random integer to represent the length of list b.
 
-a = random.sample(range(1, 100), a_len)
-b = random.sample(range(1, 100), b_len)
+
+a = random.sample(range(1, 101), a_len)     # Generate a list of a_len length with random integers in range 1-100.
+b = random.sample(range(1, 101), b_len)     # Generate a list of b_len length with random integers in range 1-100.
 
 common_elements = []
 
 for element_A in a:
-    if element_A in common_elements:
+    if element_A in common_elements:    # If an element is already inserted in the answer list, do not parse it.
         continue
     for element_B in b:
-        if element_A == element_B:
-            common_elements.append(element_A)
+        if element_A == element_B:      # Found a common element not already in answer list.
+            common_elements.append(element_A)   # Add it to the answer list.
 
 print("Random generated list a: " + str(a))
 print("Random generated list b: " + str(b))
