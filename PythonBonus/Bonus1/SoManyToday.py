a018408ba76713dbccd9bf715003eef3c0211536
Zois Zogopoulos <zoiszogop@gmail.com>
2019-11-26 15:07:16 +0200

    develop1_v4
    -Reorganized code in file Tools.py.
    -Added setters and getters for global variables.
    -Reworked working dates, weekends and holidays.
    -The program can now produce valis results even on non working dates and hours.

--- PythonBonus/Bonus1/SoManyToday.py
+++ PythonBonus/Bonus1/SoManyToday.py
@@ -6,14 +6,14 @@ from datetime import timedelta
 from PythonBonus.Bonus1.Tools import *
 
 now = datetime.datetime.today()       # Get current date-time.
-first_day_date = datetime.datetime(2019, 11, 13, 9)
-# Specify starting date of internship (work starts at 9:00 o'clock in the morning).
-last_day_date = datetime.datetime(2020, 5, 13, 9)  # Specify ending date of internship.
-work_dates = get_working_dates(first_day_date, last_day_date)
-# Get all working dates included,  between first_day_date and last_day_date.
+first_day_date = datetime.datetime(2019, 11, 13, 9)     # Specify starting date of internship.
+last_day_date = datetime.datetime(2020, 5, 13, 9)       # Specify ending date of internship.
+set_dates(first_day_date, last_day_date)                # Sets working dates, weekends
+set_holidays()                                          # and holidays.
+set_working_status(now)                                 # Sets working_status True if working, false if not.
 
 total_to_be_paid = (580.8 - 580.8*0.2976)*6     # Total pay for 6 month internship.
-pay_per_microsecond_8 = total_to_be_paid / (len(work_dates) * 8 * 60 * 60 * 1000 * 1000)
+pay_per_microsecond_8 = total_to_be_paid / (len(working_dates) * 8 * 60 * 60 * 1000 * 1000)
 # Calculate pay per microsecond working 8 hours a day.
 
 so_many_today = last_day_date + timedelta(hours=8) - now    # Generate date difference.
@@ -25,20 +25,24 @@ print("\nΗ πρακτική μου τελειώνει σε " + str(tfl[0]) + " 
       + str(tfl[6]) + " χιλιοστά του δευτερολέπτου και "
       + str(tfl[7]) + " μικροδευτερόλεπτα.")
 
-time_worked_today = now - datetime.datetime(now.year, now.month, now.day, 9)    # Calculate working time today.
-microseconds_passed_today = time_worked_today.total_seconds() * (10 ** 6)     # Convert to microseconds.
-euro_made_today = (microseconds_passed_today * pay_per_microsecond_8)
-print("\nΣήμερα έχω ήδη βγάλει " + str(euro_made_today) + " ευρώ.")
+if get_working_status():    # If I am working right now, calculate pay earned today.
+    time_worked_today = now - datetime.datetime(now.year, now.month, now.day, 9)    # Calculate working time today.
+    microseconds_passed_today = time_worked_today.total_seconds() * (10 ** 6)     # Convert to microseconds.
+    euro_made_today = (microseconds_passed_today * pay_per_microsecond_8)
+    print("\nΣήμερα έχω ήδη βγάλει " + str(euro_made_today) + " ευρώ.")
+elif not get_working_status():
+    print("\nΣήμερα δεν δουλεύω...Ουχουυυυυυυυυ.")
+
 
 time_worked = 0
-for day in work_dates:  # For each working day, calculate the sum of working seconds.
+for day in working_dates:  # For each working day, calculate the sum of working seconds.
     if day.year == now.year and day.month == now.month and day.day == now.day:
         break   # For today working seconds are already calculated.
     time_worked += 8*60*60
-time_worked += time_worked_today.total_seconds()    # Add working seconds for today.
+if get_working_status():    # If I am working right now, add pay earned today to total pay until previous pay day.
+    time_worked += time_worked_today.total_seconds()    # Add working seconds for today.
+elif not get_working_status() and now.date in working_dates:    # If I am not working right now, but I will or I was,
+    time_worked += time_worked_today                            # add pay earned today to total pay.
 microseconds_passed = time_worked*(10**6)   # Convert to microseconds.
 euro_made = (microseconds_passed * pay_per_microsecond_8)
 print("Συνολικά έχω βγάλει " + str(euro_made) + " ευρώ.")
-
-
-
--- PythonBonus/Bonus1/Tools.py
+++ PythonBonus/Bonus1/Tools.py
@@ -1,21 +1,50 @@
 import datetime
 
+working_dates = []
+weekends = []
+holidays = []
+working_status = True
 
-def get_working_dates(work_start, work_end):
-    working_dates = []
-    holidays = [datetime.date(2019, 12, 25),
-                datetime.date(2020, 3, 25),
-                datetime.date(2020, 4, 13),
-                datetime.date(2020, 5, 1),
-                ]
+
+def get_working_dates():        #####################################
+    return working_dates        #                                   #
+                                #                                   #
+                                #                                   #
+def get_weekends():             #                                   #
+    return weekends             #       Function getters for        #
+                                #         global variables          #
+                                #                                   #
+def get_holidays():             #                                   #
+    return holidays             #                                   #
+                                #                                   #
+                                #                                   #
+def get_working_status():       #                                   #
+    return working_status       #####################################
+
+
+def set_dates(work_start, work_end):
 
     date_generated = [work_start + datetime.timedelta(days=x) for x in range(0, (work_end - work_start).days)]
-
+    # Generate all dates between first day and last day at work.
     for date in date_generated:
-        if date.weekday() in (5, 6) or date.date() in holidays:
+        if date.weekday() in (5, 6):        # Append weekends to specified list.
+            weekends.append(date)
+        elif date.weekday() in holidays:    # Ignore holidays.
             continue
-        working_dates.append(date)
-    return working_dates
+        else:                               # Append working dates to specified list.
+            working_dates.append(date)      # Weekends and holidays are not included.
+
+
+def set_working_status(now):
+    global working_status
+    for day in holidays:                # No work during holidays.
+        if now.date() == day:
+            working_status = False
+    for day in weekends:                # No work during weekends.
+        if now.date() == day.date():
+            working_status = False
+    if now.hour < 9 or now.hour > 15:   # No work yet or worked already.
+        working_status = False
 
 
 def time_left(so_many_today):
@@ -24,20 +53,29 @@ def time_left(so_many_today):
     seconds_left = so_many_today.seconds             # Generate specified values, according to calculated difference. #
     micro_seconds_left = so_many_today.microseconds  ##################################################################
 
-    years_left = days_left // 365                   #########################################################
-    days_left -= years_left * 365                   #                                                       #
-    months_left = days_left // 30                   #                                                       #
-    days_left -= months_left * 30                   #                                                       #
-    weeks_left = days_left // 7                     #                                                       #
-    days_left -= weeks_left * 7                     #   Generate specified time periods with simple math    #
-    hours_left = seconds_left // 60 // 60           #                                                       #
-    seconds_left -= 60 * 60 * hours_left            #                                                       #
-    minutes_left = seconds_left // 60               #                                                       #
-    seconds_left -= 60 * minutes_left               #                                                       #
-    mil_seconds_left = micro_seconds_left // 1000   #                                                       #
-    micro_seconds_left -= 1000 * mil_seconds_left   #########################################################
+    years_left = days_left // 365                    #########################################################
+    days_left -= years_left * 365                    #                                                       #
+    months_left = days_left // 30                    #                                                       #
+    days_left -= months_left * 30                    #                                                       #
+    weeks_left = days_left // 7                      #                                                       #
+    days_left -= weeks_left * 7                      #   Generate specified time periods with simple math    #
+    hours_left = seconds_left // 60 // 60            #                                                       #
+    seconds_left -= 60 * 60 * hours_left             #                                                       #
+    minutes_left = seconds_left // 60                #                                                       #
+    seconds_left -= 60 * minutes_left                #                                                       #
+    mil_seconds_left = micro_seconds_left // 1000    #                                                       #
+    micro_seconds_left -= 1000 * mil_seconds_left    #########################################################
     tfl.extend([years_left, months_left, weeks_left, days_left,
                 hours_left, minutes_left, seconds_left, mil_seconds_left, micro_seconds_left])
     # Add above time values to tfl list.
 
     return tfl
+
+
+def set_holidays():
+    global holidays
+    holidays = [datetime.date(2019, 12, 25),
+                datetime.date(2020, 3, 25),
+                datetime.date(2020, 4, 13),
+                datetime.date(2020, 5, 1),
+                ]
