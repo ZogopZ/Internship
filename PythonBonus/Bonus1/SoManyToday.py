e78c85ce364a456a36f8a4065947c01638cd7f6a
Zois Zogopoulos <zoiszogop@gmail.com>
2019-11-29 13:07:34 +0200

    develop1_v6
    -Added automatic email functionality.
    -Implemented password encryption.

--- PythonBonus/Bonus1/SoManyToday.py
+++ PythonBonus/Bonus1/SoManyToday.py
@@ -4,47 +4,56 @@
 import datetime
 from datetime import timedelta
 from PythonBonus.Bonus1.Tools import *
+# Change above line with: from Tools import *
+# if you want to run it one line from a terminal.
 
-now = datetime.datetime.today()       # Get current date-time.
+now = datetime.datetime.today()  # Get current date-time.
 print("\n" + str(now))
-first_day_date = datetime.datetime(2019, 11, 13, 9)     # Specify starting date of internship.
-last_day_date = datetime.datetime(2020, 5, 13, 9)       # Specify ending date of internship.
-set_dates(first_day_date, last_day_date)                # Sets working dates, weekends
-set_holidays()                                          # and holidays.
-set_working_status(now)                                 # Sets working_status True if working, false if not.
+first_day_date = datetime.datetime(2019, 11, 13, 9)  # Specify starting date of internship.
+last_day_date = datetime.datetime(2020, 5, 13, 9)  # Specify ending date of internship.
+set_dates(first_day_date, last_day_date)  # Sets working dates, weekends
+set_holidays()  # and holidays.
+set_working_status(now)  # Sets working_status True if working, false if not.
 
-total_to_be_paid = (580.8 - 580.8*0.2976)*6     # Total pay for 6 month internship.
+total_to_be_paid = (580.8 - 580.8 * 0.2976) * 6  # Total pay for 6 month internship.
 pay_per_microsecond_8 = total_to_be_paid / (len(working_dates) * 8 * 60 * 60 * 1000 * 1000)
+pay_per_hour_8 = total_to_be_paid / (len(working_dates) * 8)
 # Calculate pay per microsecond working 8 hours a day.
 
-so_many_today = last_day_date + timedelta(hours=8) - now    # Generate date difference.
+so_many_today = last_day_date + timedelta(hours=8) - now  # Generate date difference.
 tfl = time_left(so_many_today)  # This function returns time left in multiple time types.
-print("Η πρακτική μου τελειώνει σε " + str(tfl[0]) + " χρόνια "
-      + str(tfl[1]) + " μήνες " + str(tfl[2]) + " εβδομάδες "
-      + str(tfl[3]) + " ημέρες " + str(tfl[3]) + " ώρες "
-      + str(tfl[4]) + " λεπτά " + str(tfl[5]) + " δευτερόλεπτα "
-      + str(tfl[6]) + " χιλιοστά του δευτερολέπτου και "
-      + str(tfl[7]) + " μικροδευτερόλεπτα.")
+output_1 = "Η πρακτική μου τελειώνει σε " + str(tfl[0]) + " χρόνια " \
+           + str(tfl[1]) + " μήνες " + str(tfl[2]) + " εβδομάδες " \
+           + str(tfl[3]) + " ημέρες " + str(tfl[3]) + " ώρες " \
+           + str(tfl[4]) + " λεπτά " + str(tfl[5]) + " δευτερόλεπτα " \
+           + str(tfl[6]) + " χιλιοστά του δευτερολέπτου και " \
+           + str(tfl[7]) + " μικροδευτερόλεπτα.\r"
+print(output_1, flush=True)
 
-
-if get_working_status():    # If I am working right now, calculate pay earned today.
-    time_worked_today = now - datetime.datetime(now.year, now.month, now.day, 9)    # Calculate working time today.
-    microseconds_passed_today = time_worked_today.total_seconds() * (10 ** 6)     # Convert to microseconds.
+output_2 = ""
+if get_working_status():  # If I am working right now, calculate pay earned today.
+    time_worked_today = now - datetime.datetime(now.year, now.month, now.day, 9)  # Calculate working time today.
+    microseconds_passed_today = time_worked_today.total_seconds() * (10 ** 6)  # Convert to microseconds.
     euro_made_today = (microseconds_passed_today * pay_per_microsecond_8)
-    print("Σήμερα έχω ήδη βγάλει " + str(euro_made_today) + " ευρώ.")
+    output_2 = "Σήμερα έχω ήδη βγάλει " + str(euro_made_today) + " ευρώ.\r"
+    print(output_2, flush=True)
 elif not get_working_status():
-    print("\nΣήμερα δεν δουλεύω...Ουχουυυυυυυυυ.")
-
+    output_2 = "\nΣήμερα δεν δουλεύω...Ουχουυυυυυυυυ.\r"
+    print(output_2, flush=True)
 
 time_worked = 0
 for day in working_dates:  # For each working day, calculate the sum of working seconds.
     if day.year == now.year and day.month == now.month and day.day == now.day:
-        break   # For today working seconds are already calculated.
-    time_worked += 8*60*60
-if get_working_status():    # If I am working right now, add pay earned today to total pay until previous pay day.
-    time_worked += time_worked_today.total_seconds()    # Add working seconds for today.
-elif not get_working_status() and now.date in working_dates:    # If I am not working right now, but I will or I was,
-    time_worked += time_worked_today                            # add pay earned today to total pay.
-microseconds_passed = time_worked*(10**6)   # Convert to microseconds.
+        break  # For today working seconds are already calculated.
+    time_worked += 8 * 60 * 60
+if get_working_status():  # If I am working right now, add pay earned today to total pay until previous pay day.
+    time_worked += time_worked_today.total_seconds()  # Add working seconds for today.
+elif not get_working_status() and now.date in working_dates:  # If I am not working right now, but I will or I was,
+    time_worked += time_worked_today  # add pay earned today to total pay.
+microseconds_passed = time_worked * (10 ** 6)  # Convert to microseconds.
 euro_made = (microseconds_passed * pay_per_microsecond_8)
-print("Συνολικά έχω βγάλει " + str(euro_made) + " ευρώ.")
+output_3 = "Συνολικά έχω βγάλει " + str(euro_made) + " ευρώ.\r"
+print(output_3, flush=True)
+
+zois_email = output_1 + output_2 + output_3 + "\r\n\r\nThis is an automated email from Python.\r"
+send_email(zois_email)
--- PythonBonus/Bonus1/Tools.py
+++ PythonBonus/Bonus1/Tools.py
@@ -1,4 +1,7 @@
 import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
 working_dates = []
 weekends = []
@@ -23,27 +26,26 @@ def get_working_status():       #                                   #
 
 
 def set_dates(work_start, work_end):
-
     date_generated = [work_start + datetime.timedelta(days=x) for x in range(0, (work_end - work_start).days)]
     # Generate all dates between first day and last day at work.
     for date in date_generated:
-        if date.weekday() in (5, 6):        # Append weekends to specified list.
+        if date.weekday() in (5, 6):  # Append weekends to specified list.
             weekends.append(date)
-        elif date.weekday() in holidays:    # Ignore holidays.
+        elif date.weekday() in holidays:  # Ignore holidays.
             continue
-        else:                               # Append working dates to specified list.
-            working_dates.append(date)      # Weekends and holidays are not included.
+        else:  # Append working dates to specified list.
+            working_dates.append(date)  # Weekends and holidays are not included.
 
 
 def set_working_status(now):
     global working_status
-    for day in holidays:                # No work during holidays.
+    for day in holidays:  # No work during holidays.
         if now.date() == day:
             working_status = False
-    for day in weekends:                # No work during weekends.
+    for day in weekends:  # No work during weekends.
         if now.date() == day.date():
             working_status = False
-    if now.hour < 9 or now.hour > 15:   # No work yet or worked already.
+    if now.hour < 9 or now.hour > 15:  # No work yet or worked already.
         working_status = False
 
 
@@ -53,18 +55,18 @@ def time_left(so_many_today):
     seconds_left = so_many_today.seconds             # Generate specified values, according to calculated difference. #
     micro_seconds_left = so_many_today.microseconds  ##################################################################
 
-    years_left = days_left // 365                    #########################################################
-    days_left -= years_left * 365                    #                                                       #
-    months_left = days_left // 30                    #                                                       #
-    days_left -= months_left * 30                    #                                                       #
-    weeks_left = days_left // 7                      #                                                       #
-    days_left -= weeks_left * 7                      #   Generate specified time periods with simple math    #
-    hours_left = seconds_left // 60 // 60            #                                                       #
-    seconds_left -= 60 * 60 * hours_left             #                                                       #
-    minutes_left = seconds_left // 60                #                                                       #
-    seconds_left -= 60 * minutes_left                #                                                       #
-    mil_seconds_left = micro_seconds_left // 1000    #                                                       #
-    micro_seconds_left -= 1000 * mil_seconds_left    #########################################################
+    years_left = days_left // 365                       #########################################################
+    days_left -= years_left * 365                       #                                                       #
+    months_left = days_left // 30                       #                                                       #
+    days_left -= months_left * 30                       #                                                       #
+    weeks_left = days_left // 7                         #                                                       #
+    days_left -= weeks_left * 7                         #   Generate specified time periods with simple math    #
+    hours_left = seconds_left // 60 // 60               #                                                       #
+    seconds_left -= 60 * 60 * hours_left                #                                                       #
+    minutes_left = seconds_left // 60                   #                                                       #
+    seconds_left -= 60 * minutes_left                   #                                                       #
+    mil_seconds_left = micro_seconds_left // 1000       #                                                       #
+    micro_seconds_left -= 1000 * mil_seconds_left       #########################################################
     tfl.extend([years_left, months_left, weeks_left, days_left,
                 hours_left, minutes_left, seconds_left, mil_seconds_left, micro_seconds_left])
     # Add above time values to tfl list.
@@ -79,3 +81,44 @@ def set_holidays():
                 datetime.date(2020, 4, 13),
                 datetime.date(2020, 5, 1),
                 ]
+
+
+def send_email(zois_email):
+    msg = MIMEMultipart()  # Create message object instance.
+    # recipients = ["theonzwg@gmail.com", "tzogx@hotmail.com", "Marianna.Leventi@ruhr-uni-bochum.de",
+    #               "zwisss@hotmail.com"]
+    msg['From'] = "zwisss@hotmail.com"  # Setup the parameters of the message.
+    # msg['To'] = ", ".join(recipients)
+    # msg['To'] = "zwisss@hotmail.com"
+    msg['Subject'] = "Ολατακοιτωσανπαιδικουτο"
+    msg.attach(MIMEText(zois_email, 'plain'))  # Add in the message body.
+
+    server = smtplib.SMTP("smtp.live.com", 587)
+    server.ehlo()  # Hostname to send for this command defaults to the fully qualified domain name of the local host.
+    server.starttls()  # Puts connection to SMTP server in TLS mode
+    server.ehlo()
+    password_file_1 = open('/home/zois/Documents/classified/spamMail/mail1')
+    # password_file_1 = open('mail1') # For better encrypting your files.
+    password_file_2 = open('/home/zois/Documents/classified/spamMail/mail2')
+    # password_file_2 = open('mail2') # For better encrypting your files.
+    password_extracted_1 = password_file_1.read()
+    password_extracted_2 = password_file_2.read()
+    password = (password_extracted_1[0] + "o" +
+                password_extracted_1[5] + "o" +
+                password_extracted_1[9] +
+                password_extracted_1[13] + "a" +
+                password_extracted_1[18] +
+                password_extracted_2[2] +
+                password_extracted_2[7] +
+                password_extracted_2[13] +
+                password_extracted_1[22])
+    server.login('zwisss@hotmail.com', password)  # Login Credentials.
+    server.sendmail(msg['From'], "zwisss@hotmail.com", msg.as_string())  # Send the message via the server.
+    print("\nMail to zwisss@hotmail.com was successfully sent.")
+    # server.sendmail(msg['From'], "theonzwg@gmail.com", msg.as_string())
+    # print("Mail to theonzwg@gmail.com was successfully sent.")
+    # server.sendmail(msg['From'], "Marianna.Leventi@ruhr-uni-bochum.de", msg.as_string())
+    # print("Mail to Marianna.Leventi@ruhr-uni-bochum.de was successfully sent.")
+    # server.sendmail(msg['From'], "tzogx@hotmail.com", msg.as_string())
+    # print("Mail to tzogx@hotmail.com was successfully sent.")
+    server.quit()
