f7b586c106e7d1d02fbd891e76f27fe34c07b361
Zois Zogopoulos <zoiszogop@gmail.com>
2020-01-29 16:57:12 +0200

    develop_v13
    -29/1/2020 updated todos.

--- utilities/good-morning.sh
+++ utilities/good-morning.sh
@@ -7,7 +7,8 @@ user="$(sudo users)"
 # Start (Do not edit this line!!!)
 echo -e "[2;90mInit       |[0;90m Good morning to you too! Let's start, shall we?[0m"
 echo -e "[2;90mInit       |[0;90m                    |************************************************************************************|[0m"
-echo -e "[2;90mInit       |[0;90m Message of the day:|*** say 1 say                                                                    ***|[0m"
+echo -e "[2;90mInit       |[0;90m Message of the day:|*** Keep populating docx tables with values. Should web vulnerabilities tracker  ***|[0m"
+echo -e "[2;90mInit       |[0;90m                    |*** have a workflow? Ask!                                                        ***|[0m"
 echo -e "[2;90mInit       |[0;90m                    |************************************************************************************|[0m"
 # End (Do not edit this line!!!)
 
