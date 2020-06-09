b589e9d4806a8cb8764a4a8767a1c9a3e91547cd
Zois Zogopoulos <z.zogopoulos@neurosoft.gr>
2020-06-09 10:33:15 +0300

    develop2_v4
    -Reworked comments and code style to match PEP8 conventions.

--- PythonExercises/Exercise2/exercise2.py
+++ PythonExercises/Exercise2/exercise2.py
@@ -1,30 +1,44 @@
-
 # Exercise 2
 
 import re
 
-pattern = r'^[-+]?[0-9]+$'  # Pattern to find positive or negative integer.
+pattern = r'^[-+]?[0-9]+$'  # Pattern for positive or negative integer.
 exit_keys = ('n', 'e', 'ex', 'exi', 'exit')  # Use these keys to exit.
-
-while True:  # Loop forever or until exit key is typed.
-    user_input = input('\nPlease, type an integer number and press enter:' + 15 * ' ' + '|Use these keys to exit: (n, e, ex, exi, exit)|' + '\033[61D')  # Wait for user input. '\033[61D' moves the cursor 61 positions to the left.
-    if user_input in exit_keys:  # User just entered on of the exit sequences. ('n', 'e', 'ex', 'exi', 'exit')
-        user_input_on_exit = input('\nYou just pressed one of the exit keys (n, e, ex, exi, exit)...\nAre you sure you want to exit (y/n)? ')
+while True:
+    # Wait for user input.
+    # '\033[61D' moves the cursor 61 positions to the left.
+    user_input = input('\nPlease, type an integer number and press enter:' +
+                       15*' ' +
+                       '|Use these keys to exit: ' +
+                       str(exit_keys) +
+                       '|' +
+                       '\033[61D')
+    # User just entered on of the exit sequences.
+    # 'n', 'e', 'ex', 'exi', 'exit'.
+    if user_input in exit_keys:
+        user_input_on_exit = input('\nYou just pressed one of the exit keys '
+                                   '(n, e, ex, exi, exit)...\n'
+                                   'Are you sure you want to exit (y/n)? ')
         if user_input_on_exit == 'y':
             print('Exiting...')
             print(100 * '_')
             break
         else:
             continue
-    elif re.match(pattern, user_input):  # User input, matches pattern (namely is an integer).
+    # User input, matches 'pattern' (namely is an integer).
+    elif re.match(pattern, user_input):
         int_value = int(user_input)  # Convert to int type.
-        if int_value % 2 == 0:  # Integer is even.
+        # Integer is even.
+        if int_value % 2 == 0:
             print('You just typed an even integer.')
             if int_value % 4 == 0:  # Integer is even and a multiple of 4.
                 print('This integer is a multiple of 4.')
-        else:  # Integer is odd.
+        # Integer is odd.
+        else:
             print('You just typed an odd integer.')
         print(100 * '_')
-    else:  # User input is not valid integer type.
-        print('The value you just typed is not an integer. Please, try again.')
+    # User input is not valid integer type.
+    else:
+        print('The value you just typed is not an integer. '
+              'Please, try again.')
 
