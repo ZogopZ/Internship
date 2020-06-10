e5205618548f91fc26f42e1bf97e405b8feb66ae
Zois Zogopoulos <z.zogopoulos@neurosoft.gr>
2020-06-10 09:47:25 +0300

    spam_mail-develop_v21
    -Renamed python files to match PEP8 conventions.
    -Reworked code and comments in python files to match PEP8 conventions.
    -Fixed date and salary bugs created by Corona-Virus absence.
    -Fixed 'spam-mail.sh' bash script bug, created by renaming python files.

--- utilities/spam-mail.sh
+++ utilities/spam-mail.sh
@@ -4,4 +4,4 @@ source /home/zois/Documents/Internship/utilities/tools # Include tools in this f
 
 ttyCurrent=$(tty)
 echo -e "${UserDim}User       |${User} Spamming mails using python...${ColorOff}"
-echo $(python3 /home/zois/Documents/Internship/utilities/assets/spamMail/SoManyToday.py > $ttyCurrent)
+echo $(python3 /home/zois/Documents/Internship/utilities/assets/spamMail/so_many_today.py > $ttyCurrent)
